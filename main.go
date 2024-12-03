0e920a6214342b807e484af16a680f55ac207656
sakurakotubaki <gitonly543@gmail.com>
2024-12-03 11:11:14 +0900

    feat: http get

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
+func main() {
+	server := gin.Default()
+
+	server.GET("/events", getEvents)
+
+	server.Run(":8080")
+}
+
+func getEvents(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello World"})
 }
