165ed088e1b73cc3bcba1666718775113db03f92
sakurakotubaki <gitonly543@gmail.com>
2024-12-03 19:13:13 +0900

    feat: signup

---
+++ routes/users.go
@@ -0,0 +1,28 @@
+package routes
+
+import (
+	"net/http"
+	"udemy-rest-api/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func signup(context *gin.Context) {
+	var user models.User
+
+	err := context.ShouldBindJSON(&user)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request body"})
+		return
+	}
+
+	err = user.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create user"})
+		return
+	}
+
+	context.JSON(http.StatusCreated, gin.H{"message": "User created successfully", "user": user})
+}
