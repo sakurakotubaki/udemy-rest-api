0602feee12a68e18d8ba9b49afffe977a7dd7c8a
sakurakotubaki <gitonly543@gmail.com>
2024-12-04 08:39:28 +0900

    perf: Group

--- routes/routes.go
+++ routes/routes.go
@@ -9,9 +9,16 @@ import (
 func RefiterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvents)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", middlewares.Authenticate, createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	authenticated := server.Group("/")
+	authenticated.Use(middlewares.Authenticate)
+	authenticated.POST("/events", createEvent)
+	authenticated.PUT("/events/:id", updateEvent)
+	authenticated.DELETE("/events/:id", deleteEvent)
+
+	// server.POST("/events", middlewares.Authenticate, createEvent)
+	// server.PUT("/events/:id", updateEvent)
+	// server.DELETE("/events/:id", deleteEvent)
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
