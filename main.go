86f3eabfa8a41325fc541b80c575d5719d5402f9
sakurakotubaki <gitonly543@gmail.com>
2024-12-03 15:39:15 +0900

    feat: http get id

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"net/http"
+	"strconv"
 
-	"github.com/gin-gonic/gin"
 	"udemy-rest-api/db"
 	"udemy-rest-api/models"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -13,14 +15,36 @@ func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.GET("/events/:id", getEvent)
 	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	events := models.GetAllEvents()
+	events, err := models.GetAllEvents()
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch events"})
+		return
+	}
 	context.JSON(http.StatusOK, events)
+}
+
+func getEvent(context *gin.Context) {
+	id := context.Param("id")
+	eventId, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Invalid event ID"})
+		return
+	}
+
+	event, err := models.GetEventById(eventId)
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not fetch event"})
+		return
+	}
+
+	context.JSON(http.StatusOK, event)
 }
 
 func createEvent(context *gin.Context) {
@@ -35,5 +59,23 @@ func createEvent(context *gin.Context) {
 	event.ID = 1
 	event.UserID = 1
 	event.Save()
+
+	if err != nil {
+		context.JSON(http.StatusInternalServerError, gin.H{"message": "Could not create event"})
+		return
+	}
+
 	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
+}
+
+func GetEventById(id int64) *models.Event {
+	query := "SELECT * FROM events WHERE id = ?"
+	row := db.DB.QueryRow(query, id)
+
+	var event models.Event
+	err := row.Scan(&event.ID, &event.Name, &event.Description, &event.Location, &event.DateTime, &event.UserID)
+	if err != nil {
+		return nil
+	}
+	return &event
 }
