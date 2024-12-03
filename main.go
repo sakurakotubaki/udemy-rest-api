59441ba8dffb5bcc51cfc60508b1e47c2cd00585
sakurakotubaki <gitonly543@gmail.com>
2024-12-03 12:07:27 +0900

    feat: http post

--- main.go
+++ main.go
@@ -3,16 +3,34 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"udemy-rest-api/models"
 )
 
 func main() {
 	server := gin.Default()
 
 	server.GET("/events", getEvents)
+	server.POST("/events", createEvent)
 
 	server.Run(":8080")
 }
 
 func getEvents(context *gin.Context) {
-	context.JSON(http.StatusOK, gin.H{"message": "Hello World"})
+	events := models.GetAllEvents()
+	context.JSON(http.StatusOK, events)
+}
+
+func createEvent(context *gin.Context) {
+	var event models.Event
+	err := context.ShouldBindJSON(&event)
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"message": "Could not parse request body"})
+		return
+	}
+
+	event.ID = 1
+	event.UserID = 1
+	event.Save()
+	context.JSON(http.StatusCreated, gin.H{"message": "Event created successfully", "event": event})
 }
